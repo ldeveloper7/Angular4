92eb4b4b5589f869c8655fb8531661fa656375a4
meghjoshi <meghna@blavity.com>
2018-04-09 16:30:18 +0530

    added 4 componants

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
+import { InsertComponent } from './insert/insert.component';
+import { UpdateComponent } from './update/update.component';
+import { ViewComponent } from './view/view.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InsertComponent,
+    UpdateComponent,
+    ViewComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
